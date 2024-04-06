f2bac00cf5f6aadd883361f335356294ab223c58
node8 <purep@qq.com>
2024-04-06 05:43:45 +0000

    使用管道得到标准输出

--- utils/avg_insert_time.py
+++ utils/avg_insert_time.py
@@ -1,6 +1,15 @@
 import os
 import subprocess
 
-os.chdir('ARTSynchronized')
-command = 'bash run_example.sh'
-subprocess.run(command, shell=True)
+def run():
+    filename = 'run_example.sh'
+    # 将标准输出重定向到管道中
+    process = subprocess.Popen(['bash', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    # communicate() 方法用于与子进程进行交互，它会等待子进程完成，并返回一个元组，其中包含子进程的标准输出和标准错误输出
+    stdout, _ = process.communicate()
+    return stdout.decode('utf-8')
+
+
+if __name__ == '__main__':
+    os.chdir('ARTSynchronized')
+    print(run())
